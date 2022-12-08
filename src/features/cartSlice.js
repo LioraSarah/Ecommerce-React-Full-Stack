b12ffad259f6e1a0920f38be1efef0363181553b
LioraSarah <liorasarahferrero@gmail.com>
2022-12-08 13:12:08 +0200

    added remove from cart

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -6,17 +6,22 @@ export const cartSlice = createSlice({
       items: []
     },
     reducers: {
+        loadCart(state, action) {
+            return(action.payload);
+        },
         addItem(state, action) {
             console.log(action.payload)
             state.items.push(action.payload);
         },
-        // removeItem(state, action) {
-        //     state.items.pop(action.payload);
-        // }
+        removeItem(state, action) {
+            const index = state.items.indexOf(action.payload);
+            if (index !== -1) {
+                state.items.splice(index, 1);
+            }
+        }
     }
 });
 
 
 export const selectCartItems = (state) => state.cart.items;
-export const { addItem } = cartSlice.actions;
-export default cartSlice.reducer;
+export const { loadCart, addItem, removeItem } = cartSlice.actions;
